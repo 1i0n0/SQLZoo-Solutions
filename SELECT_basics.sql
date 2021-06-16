0896a2b29932bf68bf735356da913510107f7e46
1i0n0 <85225837+1i0n0@users.noreply.github.com>
2021-06-16 17:01:43 +0800

    Update SELECT_basics.sql
    Remove some empty lines.

--- SELECT_basics.sql
+++ SELECT_basics.sql
@@ -3,19 +3,16 @@
 -- ----------------------------
 
 --1. Show the population of Germany.
-
 SELECT population 
 FROM world
 WHERE name = 'Germany';
 
 --2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
-
 SELECT name, population 
 FROM world
 WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 --3. Show the country and the area for countries with an area between 200,000 and 250,000.
-
 SELECT name, area 
 FROM world
 WHERE area BETWEEN 200000 AND 250000;
@@ -32,7 +29,6 @@ Bahrain		1234571
 Swaziland	1220000
 Timor-Leste	1066409
 */
-
 SELECT name, population
 FROM world
 WHERE population BETWEEN 1000000 AND 1250000;
@@ -49,7 +45,6 @@ Algeria	32900000
 
 
 --3. Select the code which shows the countries that end in A or L.
-
 SELECT name 
 FROM world
 WHERE name LIKE '%a' OR name LIKE '%l';
@@ -86,7 +81,6 @@ Andorra	936
 
 --6. Select the code that would show the countries with an area larger than 50000 
 --   and a population smaller than 10000000.
-
 SELECT name, area, population
 FROM world
 WHERE area > 50000 AND population < 10000000;
